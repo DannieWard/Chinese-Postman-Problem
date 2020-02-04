e7d1f12ca1fa128a3014b7a11fa410ce8f1500ff
ajbenz18 <60668605+ajbenz18@users.noreply.github.com>
2020-02-04 14:22:42 -0500

    Update China.py

--- China.py
+++ China.py
@@ -1,7 +1,25 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Feb  4 13:45:28 2020
+import networkx as nx
+import random
+import matplotlib.pyplot as plt
 
-@author: danni
-"""
-poop
+nodeList=[]
+numberOfNodes=random.randint(8, 12)
+alphabet='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+G=nx.Graph()
+print(numberOfNodes)
+print(type(list(G.nodes)))
+for i in range(numberOfNodes):
+    G.add_node(alphabet[i])
+nx.draw(G, with_labels=True)
+plt.show(G)
+for i in G.nodes:
+    r=random.randint(0,1)
+    if r==0:
+        r=2
+    else:
+        r=4
+    for j in range(4):
+        G.add_edge(i, list(G.nodes)[random.randint(0, numberOfNodes-1)])
+print(G.edges)
+nx.draw(G, with_labels=True)
+plt.show(G)
